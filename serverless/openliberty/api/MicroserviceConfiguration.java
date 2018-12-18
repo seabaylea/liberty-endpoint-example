bc9ce246de13846e8acf9851716bb9ffde725eee
Chris Bailey <baileyc@uk.ibm.com>
2018-12-18 12:00:37 +0000

    first commit

---
+++ serverless/openliberty/api/MicroserviceConfiguration.java
@@ -0,0 +1,9 @@
+package serverless.openliberty.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/server")
+public class MicroserviceConfiguration extends Application {
+
+}
---
+++ serverless/openliberty/api/PingPong.java
@@ -0,0 +1,15 @@
+package serverless.openliberty.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/ping")
+public class PingPong {
+
+    @GET
+    public Response ping() {
+        return Response.ok("Pong")
+                .build();
+    }
+}
